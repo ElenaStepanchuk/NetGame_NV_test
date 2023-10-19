f7dbbb8b70a7ddf86a2538f83ef320a2c8e98ae7
Elena <podolyak365@gmail.com>
2023-10-19 16:02:24 +0300

    ship1

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const Application = PIXI.Application;
 const widthCanvas = 1280;
 const heightCanvas = 720;
 
-import Ship from "./components/ship/Ship";
+// import { Ship } from "./components/ship/Ship";
 
 const app = new Application({
   width: widthCanvas,
@@ -26,3 +26,22 @@ document.body.appendChild(app.view);
 //   shipSprite.interactive = true;
 // }
 // addingSpiceShip();
+
+class Ship {
+  constructor(imgPath, shipWidth, shipHeight, x, y) {
+    this.shipSprite = app.stage.addChild(PIXI.Sprite.from(imgPath));
+    this.shipSprite.width = shipWidth;
+    this.shipSprite.height = shipHeight;
+    this.shipSprite.x = x;
+    this.shipSprite.y = y;
+    this.shipSprite.interactive = true;
+  }
+}
+
+const shipSprite = new Ship(
+  "../images/spaceShip.png",
+  60,
+  140,
+  app.screen.width / 2 - 15,
+  570
+);
--- components/ship/Ship.js
+++ components/ship/Ship.js
@@ -1,30 +1,11 @@
 class Ship {
   constructor(imgPath, shipWidth, shipHeight, x, y) {
-    this.shipSprite = PIXI.Sprite.from(imgPath);
+    this.shipSprite = app.stage.addChild(PIXI.Sprite.from(imgPath));
     this.shipSprite.width = shipWidth;
     this.shipSprite.height = shipHeight;
     this.shipSprite.x = x;
     this.shipSprite.y = y;
     this.shipSprite.interactive = true;
   }
-
-  // const shipSprite = PIXI.Sprite.from("../images/spaceShip.png");
-  // app.stage.addChild(shipSprite);
-  //   shipSprite.width = shipSpriteWidth;
-  //   shipSprite.height = shipSpriteHeight;
-  //   shipSprite.x = app.screen.width / 2 - 15;
-  //   shipSprite.y = 570;
-  //   shipSprite.interactive = true;
 }
 export default Ship;
-
-// const shipSpriteWidth = 60;
-// const shipSpriteHeight = 140;
-// const shipSprite = new Ship(
-//   "../images/spaceShip.png",
-//   shipSpriteWidth,
-//   shipSpriteHeight,
-//   app.screen.width / 2 - 15,
-//   570
-// );
-// app.stage.addChild(shipSprite);
